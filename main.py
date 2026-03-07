92681c72b388a55cf1b6610788ce36d705e0483e
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-03-07 17:01:33 +0530

    Add difficulty level selection

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ print("Select your subject")
 print("1:GK")
 print("2:English")
 print("3:Computer Science")
-subject=input(int("Select your subject: "))
+subject=int(input("Select your subject: "))
 if subject==1:
-    print("GK quiz starting")
+    print("GK quiz starting.....")
 elif subject==2:
-    print("English quiz starting")
+    print("English quiz starting.....")
 elif subject==3:
-    print("Computer Science quiz starting")
-else
-    print("Invalid choice")
+    print("Computer Science quiz starting.....")
+else:
+    print("Invalid choice🫣")
+print("select difficulty level:")
+print("1:Easy")
+print("2:Medium")
+print("3:Hard")
+difficulty=int(input("Select your difficulty level: "))
+if difficulty==1:
+    print("Easy starting.....")
+elif difficulty==2:
+    print("Medium starting.....")
+elif difficulty==3:
+    print("Hard starting.....")
+else :
+    print("Invalid choice🫣")
